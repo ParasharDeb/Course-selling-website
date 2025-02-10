f8e7f427ad21aa3cc788b192461d854eb68053dc
ParasharDeb <parashardeb11@gmail.com>
2025-02-10 19:32:48 +0530

    Removed node_modules and dist

--- dist/Router/courses.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CourseRouter = void 0;
-const express_1 = require("express");
-exports.CourseRouter = (0, express_1.Router)();
-exports.CourseRouter.post("/purchases", (req, res) => {
-});
-exports.CourseRouter.post("/preview", (req, res) => {
-});
